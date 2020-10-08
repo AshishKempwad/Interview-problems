5e302e5ec5c204e6a8cedd4108dbdff606f61dd3
Swapnil Satpathy <swapnilsatpathycse24@gmail.com>
2020-10-08 17:40:21 +0530

    ~ Swapnil
    Updated Correct and Running Code for Stairs.

--- Amazon/stairs.cpp
+++ Amazon/stairs.cpp
@@ -2,31 +2,49 @@
 using namespace std;
 
 
-int find_no_of_stairs(int n){
+
+
+int main(){
+	int t;
+	cin>>t;
+
 	
-	int * arr=new int[n+1];
-	arr[0]=0;
-	arr[1]=1;
-	arr[2]=2;
-	int count=2;
-	for(int i=3;i<=n;i++){
-		if(i%2 !=0){
-			arr[i]=1+arr[i-count];
-			count++;
+		
+	while(t--){
+		int n;
+		cin>>n;
+		int * arr =new int[n];
+		for(int i=0;i<n;i++)
+			cin>>arr[i];
+		int * dp=new int[n+1];
+		dp[0]=0;
+		dp[1]=1;
+
+		for(int i=2;i<=n;i++){
+			dp[i]=dp[i-1]+1;
+			for(int j=i-1;j>=0;j--){
+				
+				if(j+arr[j]>=i)
+					dp[i]=min(dp[i],dp[j]+1);
+			}
 		}
-		else
-			arr[i]=1+arr[i-1];
+		
+		
+		
+		int q;
+		cin>>q;
+		while(q--){
+			int no;
+			cin>>no;
+			cout<<dp[no]<<endl;
+		}
+
+
+		delete [] dp;
+		delete [] arr;
 	}
-	return arr[n];
-
-}
+	
 
 
-
-int  main(){
-
-	int n;
-	cin>>n;
-	cout<<find_no_of_stairs(n)<<endl;
 	return 0;
 }
